1a9a580dcfdb92f99bddf34110f6360750b91cad
Kristina Evtuh <kristika2012@gmail.com>
2024-01-23 15:16:52 +0300

    Add login page class and tests

---
+++ test_login_page.py
@@ -0,0 +1,10 @@
+import selenium
+from .pages.login_page import LoginPage
+
+
+def test_guest_should_go_to_login_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+
